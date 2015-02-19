eb75dc43825279c6b921b47bfa6e5f11820b2c37
Ilya Ulyanov <ilya.wt@gmail.com>
2015-02-19 09:35:20 -0800

    index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once('model/index.php');
 $dr = new droute();
 
 $picks = $dr->get_all_staff_picks();
-print_r($picks);
+//print_r($picks);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -110,6 +110,7 @@ print_r($picks);
           <h3><i class="mdi-content-send brown-text"></i></h3>
           <h4>Compatable Drones</h4>
           <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+          <h4 class="header center text-lighten-2">Staff Picks</h4>
             <div class="row">
                 <?php
                   foreach ($picks as $card) {

f3250293335c3919b9299eb989331753c14ccc93
forceapple <life147@live.com>
2015-02-18 21:47:49 -0800

    index.php fucntion

--- model/index.php
+++ model/index.php
@@ -1,9 +1,36 @@
 <?php
 
-class droute{
-	function get_staff_pick($id){
+require("connect.php");
 
-	};
+class droute{
+	function get_all_routes(){
+		global $con;
+		$query = "SELECT * FROM routes LEFT JOIN routes_staffpicks ON routes_staffpicks.routes_id = routes.id 
+										LEFT JOIN categories ON categories.id = routes.category ";
+		$result = mysqli_query($con, $query);
+		//print_r($result);
+		if($result){
+			$arr = array();
+			$arr2 = array();
+			while($row = mysqli_fetch_array($result)){
+				$arr['id']= $row['routes_id'];
+				$arr['route_name']= $row['route_name'];
+				$arr['category_name']=  $row['category_name'];
+				$arr['add_date']=  $row['add_date'];
+				$arr2 []= $arr;
+			}
+			//print_r($arr2);
+			return $arr2;
+			
+		}
+	}
 
 }
+
+
+//$db = new droute();
+
+//$db -> get_all_routes();
+
+
 ?>

5ebadb8b042ac82b0549996ae064ee1ef6e0cd6a
Ilya Ulyanov <ilya.wt@gmail.com>
2015-02-26 15:42:26 -0800

    features page

--- features.php
+++ features.php
@@ -10,7 +10,7 @@
   <title>Droute</title>
 
   <!-- CSS  -->
-  <link href="bower_components/materialize/dist/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="bower_components/materialize/dist/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="bower_components/materialize/dist/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
@@ -31,33 +31,226 @@
       </div>
     </div>
   </nav>
+  <div class="container">
+    <div class="row">
+        <div class="col s12 m9 l10">
+          <h1 class="header">Features</h1>
+          <h2 class="header section scrollspy" id="kiip-rewards">Kiip Rewards</h2>
+          <div class="card large">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              <a href='#'>This is a link</a>
+            </div>
+          </div>
+          <h2 class="header section scrollspy" id="supported-drones">Supported Drones</h2>
+          <div class="card large">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              <a href='#'>This is a link</a>
+            </div>
+          </div>
 
-   <div class="row">
+          <h2 class="header section scrollspy" id="route-media-timeline">Route media timeline</h2>
+          <div class="card large">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              <a href='#'>This is a link</a>
+            </div>
+          </div>
 
-      <div class="col s12 m4 l3"> <!-- Note that "m4 l3" was added -->
-        <!-- Grey navigation panel
+          <h2 class="header section scrollspy" id="route-cloud-saving">Route cloud saving</h2>
+          <div class="card large">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              <a href='#'>This is a link</a>
+            </div>
+          </div>
 
-              This content will be:
-          3-columns-wide on large screens,
-          4-columns-wide on medium screens,
-          12-columns-wide on small screens  -->
+          <h2 class="header section scrollspy" id="searching-routes">Searching routes by location</h2>
+          <div class="card large">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              <a href='#'>This is a link</a>
+            </div>
+          </div>
 
-      </div>
+          <h2 class="header section scrollspy" id="media-library">Media library for uploading</h2>
+          <div class="card large">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              <a href='#'>This is a link</a>
+            </div>
+          </div>
 
-      <div class="col s12 m8 l9"> <!-- Note that "m8 l9" was added -->
-        <!-- Teal page content
+          <h2 class="header section scrollspy" id="private-repository">Private repository for routes</h2>
+          <div class="card large">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              <a href='#'>This is a link</a>
+            </div>
+          </div>
 
-              This content will be:
-          9-columns-wide on large screens,
-          8-columns-wide on medium screens,
-          12-columns-wide on small screens  -->
+          <h2 class="header section scrollspy" id="free-account">Free personal account</h2>
+          <div class="card large">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              <a href='#'>This is a link</a>
+            </div>
+          </div>
 
-      </div>
+          <h2 class="header section scrollspy" id="nearby-routes">Nearby routes using geolocation</h2>
+          <div class="card large">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              <a href='#'>This is a link</a>
+            </div>
+          </div>
+
+          <h2 class="header section scrollspy" id="cloud-hosting">Cloud hosting for photos, videos</h2>
+          <div class="card large">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              <a href='#'>This is a link</a>
+            </div>
+          </div>
+
+          <h2 class="header section scrollspy" id="follow">Follow local profiles and communities</h2>
+          <div class="card large">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              <a href='#'>This is a link</a>
+            </div>
+          </div> 
+        </div>
+
+        <div class="col hide-on-small-only m3 l2">
+          <div class="toc-wrapper white">
+            <ul class="section table-of-contents">
+              <li><a href="#kiip-rewards">Kiip rewards</a></li>
+              <li><a href="#supported-drones">Supported drones</a></li>
+              <li><a href="#route-media-timeline">Route media timeline</a></li>
+              <li><a href="#route-cloud-saving">Route cloud saving</a></li>
+              <li><a href="#searching-routes">Searching routes by location</a></li>
+              <li><a href="#media-library">Media library for uploading</a></li>
+              <li><a href="#private-repository">Private repository for routes</a></li>
+              <li><a href="#free-account">Free personal account</a></li>
+              <li><a href="#nearby-routes">Nearby routes using geolocation</a></li>
+              <li><a href="#cloud-hosting">Cloud hosting for photos, videos</a></li>
+              <li><a href="#follow">Follow local profiles and communities</a></li>
+
+            </ul>
+          </div>
+        </div>
+    </div>
+  </div>
+
+
 
     <!--  Scripts-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="bower_components/materialize/dist/js/init.js"></script>
   <script src="bower_components/materialize/dist/js/materialize.min.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $('.scrollspy').scrollSpy();
+      // Floating-Fixed table of contents
+      if ($('nav').length) {
+        $('.toc-wrapper').pushpin({ top: $('nav').height() });
+      }
+      else {
+        $('.toc-wrapper').pushpin({ top: 0 });
+      }
+    });
+        
+  </script>
 </body>
 
 </html>

b9899a8b13e83206e974b99bdc462a83d0c1c57c
forceapple <life147@live.com>
2015-03-20 21:24:27 -0800

    contact page

--- contact.php
+++ contact.php
@@ -41,14 +41,87 @@
   </div>
 
   <!-- content -->
-
-  <div class='row valign-wrapper'>
-    <div class="col l12">
-      <h3 class='center-align about-lead'>Droute tset t set tst tset s et ste t s set  tsetse  tset  tes tes t sesef  fse  fsf srtgtrtgdsadsad</h3>
+  <div class="container">
+    <div class="row">
+      <div class="col s12  ">
+        <ul class="tabs">
+          <li class="tab col s6"><a class="active" href="#about-us">About Drout</a></li>
+          <li class="tab col s6"><a href="#jobs">Jobs</a></li>
+        </ul>
+      </div>
+      <div id="about-us" class="col s12">
+        <div class='row valign-wrapper'>
+          <h4 class='center-align about-lead'>
+            Droute is the best place to share Drone taken videos and photos with friends, co-workers, classmates, and complete strangers. We are amazed how the commuinty has grown, lets do great things together.
+          </h4>
+        </div>
+        <div class="divider"></div>
+        <div class='row valign-wrapper left-align pad'>
+          <div class='col s6'>
+           <h5>With the collaborative features of Droute. It has never been easier for individuals and teams to survey an area and explore. Make an account with us and start collaborating</h5>
+          </div>
+          <div class='col s6'>
+          <h5>Founded by Ilya Ulyanov, Jeff FitzGerald, Gordon Lee to grow the drone community. The drone community is quickly becoming one of the fastest growing communities</h5>
+          </div>
+        </div>
+      </div>
+      <div id="jobs" class="col s12">
+        <div class='row valign-wrapper'>
+          <h4 class='center-align about-lead'>Come help us make collaboration even better. At Droute we build the tools that make collaborating and flying easier. We've built a company we truly love working for, and we think you will too. Here's why.</h4>
+        </div>
+        <div class="divider"></div>
+        <div class='row valign-wrapper left-align'>
+          <div class='col s6'>
+           <h5>asdasdasdasdasdasdasdasd asdasdasd asdas  adas  dasd d sa dasd asdas asd asd  asd as d dasda sdas</h5>
+          </div>
+          <div class='col s6'>
+          <h5>asdasdasdasdasdasdasdasd asdasdasd asdas  adas  dasd d sa dasd asdas asd asd  asd as d dasda sdas</h5>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
+
+
+  <footer class="page-footer dark-primary-bg">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Company Bio</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Careers</h5>
+          <ul>
+            <li><a class="white-text" href="#!">iOS Developer</a></li>
+            <li><a class="white-text" href="#!">Android Developer</a></li>
+            <li><a class="white-text" href="#!">Marketing Intern</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Gordon Lee</a></li>
+            <li><a class="white-text" href="#!">Ilya Ulyanov</a></li>
+            <li><a class="white-text" href="#!">Jeff FitzGerald</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Keelo Digital</a>
+      </div>
+    </div>
+  </footer>
+
+
   
+
+
     <!--  Scripts-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="bower_components/materialize/dist/js/init.js"></script>
--- features.php
+++ features.php
@@ -202,6 +202,40 @@
     </div>
   </div>
 
+  <footer class="page-footer dark-primary-bg">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Company Bio</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Careers</h5>
+          <ul>
+            <li><a class="white-text" href="#!">iOS Developer</a></li>
+            <li><a class="white-text" href="#!">Android Developer</a></li>
+            <li><a class="white-text" href="#!">Marketing Intern</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Gordon Lee</a></li>
+            <li><a class="white-text" href="#!">Ilya Ulyanov</a></li>
+            <li><a class="white-text" href="#!">Jeff FitzGerald</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Keelo Digital</a>
+      </div>
+    </div>
+  </footer>
+
 
 
     <!--  Scripts-->
